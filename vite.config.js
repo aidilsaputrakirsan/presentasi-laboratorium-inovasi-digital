ce1f2dbfef77bb1eb65579182dab3d770d14a64c
Myst-ADL <89290578+aidilsaputrakirsan@users.noreply.github.com>
2026-02-02 20:28:23 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: process.env.NODE_ENV === 'production' ? '/presentasi-laboratorium-inovasi-digital/' : '/',
   server: {
     port: 5173,
     open: true
